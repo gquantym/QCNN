1e2e56b5470313c1ebd55b4160525d42e79f5ede
gquantym <151845051+gquantym@users.noreply.github.com>
2024-04-25 14:34:11 +0100

    Update training.py

--- training.py
+++ training.py
@@ -9,6 +9,7 @@ def pulsar_probability(sampled_data, weights, quantum_circuit):
     return probability_pulsar
 
 def cross_entropy_loss(weights, data, quantum_circuit):
+    
     predictions = np.array([quantum_circuit.circuit(weights, feature_vector) for feature_vector in data[:,:8]])
     predictions = (1-predictions)/2
     targets = np.array([row[-1] for row in data])
@@ -17,6 +18,7 @@ def cross_entropy_loss(weights, data, quantum_circuit):
     #print("Prediction = ",predictions,"targets = ",targets)
     loss = -np.sum(targets * np.log(predictions) + (1 - targets) * np.log(1 - predictions))
     loss = loss / len(targets)
+    
     return loss
 
 def train(initial_weights, train_data, quantum_circuit,set_number,epochs):
@@ -28,8 +30,8 @@ def train(initial_weights, train_data, quantum_circuit,set_number,epochs):
     grad_fn = qml.grad(cross_entropy_loss, argnum=0)
     
     for epoch in range(epochs):
-        current_gradient = grad_fn(initial_weights, train_data, quantum_circuit)
-        gradient_array.append(current_gradient)
+        #current_gradient = grad_fn(initial_weights, train_data, quantum_circuit)
+        #gradient_array.append(current_gradient)
         optimized_weights, current_loss = optimizer.step_and_cost(lambda w: cross_entropy_loss(w, train_data, quantum_circuit), initial_weights)
         initial_weights = optimized_weights
         loss_array = np.append(loss_array, current_loss)
@@ -45,11 +47,13 @@ def train(initial_weights, train_data, quantum_circuit,set_number,epochs):
 def main(train_pulsar,train_non_pulsar,initial_weights,quantum_circuit,title,num_sets,epochs):
     train_data = np.concatenate((train_pulsar, train_non_pulsar), axis=1)
     weights_loss_values_grad = [train(initial_weights[i], train_data[i],quantum_circuit,i,epochs) for i in range(num_sets)]
+    
     opt_weights, loss_values, param_grad = zip(*weights_loss_values_grad)
     
     opt_prob_non_pulsar = [pulsar_probability(train_non_pulsar[i,:,:8],opt_weights[i],quantum_circuit)for i in range(num_sets)]
-    opt_prob_pulsar = [pulsar_probability(train_pulsar[i,:,:8],opt_weights[i],quantum_circuit) for i in range(num_sets)]
-    opt_prob_non_pulsar = np.array(opt_prob_non_pulsar)
-    opt_prob_pulsar = np.array(opt_prob_pulsar)
 
-    return opt_weights,loss_values,param_grad
+    #opt_prob_pulsar = [pulsar_probability(train_pulsar[i,:,:8],opt_weights[i],quantum_circuit) for i in range(num_sets)]
+    #opt_prob_non_pulsar = np.array(opt_prob_non_pulsar)
+    #pt_prob_pulsar = np.array(opt_prob_pulsar)
+    
+    return opt_weights,loss_values#,param_grad
